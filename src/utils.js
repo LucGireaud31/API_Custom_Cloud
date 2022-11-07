76730e1befc4fb6eaaf67e62f70199ca23d06776
luc <luc.gireaud@gmail.com>
2022-11-07 11:24:54 +0100

    format spaces in utils also with #

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 function formatPathWithSpaces(path) {
-  return path.replaceAll("\\ ", " ").replaceAll(" ", "\\ ");
+  return path.replaceAll("\\ ", "#").replaceAll(" ", "#");
 }
 
 function resetTempFiles(root) {
